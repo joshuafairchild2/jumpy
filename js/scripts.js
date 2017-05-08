28d8416bc0d690f91a8b16e7f35906cd5083072c
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-05-08 08:29:06 -0700

    create loop to render paddles randomly through the x axis. the game works in its most basic form but it needs to be reworked to properly continue infinitely and not render all of the paddles onload

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,10 @@ let GameState = {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.game.physics.arcade.gravity.y = 1000;
     this.stage.backgroundColor = 'black';
-    this.world.resize(375,6000)
+    this.world.resize(400,10000)
 
 
-    this.ball = this.add.sprite(30, 5700,'ball');
+    this.ball = this.add.sprite(this.game.width / 2, 9850,'ball');
     this.game.physics.arcade.enable(this.ball);
     this.ball.scale.setTo(0.75);
     this.ball.anchor.setTo(0.5);
@@ -29,12 +29,14 @@ let GameState = {
     this.paddles = this.game.add.group();
     this.paddles.enableBody = true;
     this.paddles.createMultiple(30,'paddle');
-    this.addPaddle(200,5600);
-    this.addPaddle(125,5750);
-    this.addPaddle(330,5800);
-    this.addPaddle(50,5900);
-  },
 
+    this.addPaddle((this.game.width / 2),9900);
+
+    for (let i = 98; i > 1; i--) {
+      this.randXCoord = this.randInt(45,355);
+      this.addPaddle(this.randXCoord,(i*100));
+    }
+  },
 
   update: function() {
     this.game.physics.arcade.collide(this.ball, this.paddles, this.bounce);
@@ -49,9 +51,10 @@ let GameState = {
   },
 
   addPaddle: function(x,y) {
-    let paddle = this.paddles.getFirstDead();
+    // let paddle = this.paddles.getFirstDead();
+    let paddle = this.paddles.create(x,y,'paddle')
 
-    paddle.reset(x,y);
+    // paddle.reset(x,y);
     paddle.checkWorldBounds = true;
     paddle.outOfBoundsKill = true;
     paddle.scale.setTo(1.5);
@@ -65,11 +68,15 @@ let GameState = {
 
   bounce: function(ball,paddles) {
     ball.body.velocity.y = -600;
+  },
+
+  randInt: function randInt(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
   }
 };
 
 
-let game = new Phaser.Game(375, 700, Phaser.AUTO);
+let game = new Phaser.Game(400, 700, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
