8d5ba704b4485afc8bdd20413c822065d8b46e1a
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-05-07 23:32:26 -0700

    apply arcade physics engine and collision detection between the ball and the paddle

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 let GameState = {
 
   init: function() {
-
   },
 
   preload: function() {
@@ -10,19 +9,27 @@ let GameState = {
   },
 
   create: function() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.arcade.gravity.y = 1000;
     this.stage.backgroundColor = 'black';
-    this.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.ball = this.add.sprite(200, 300,'ball');
+    this.game.physics.arcade.enable(this.ball);
     this.ball.scale.setTo(0.75);
     this.ball.anchor.setTo(0.5);
+    this.ball.body.bounce.y = 1;
+    this.ball.body.collideWorldBounds = true;
 
-    this.paddle = this.add.sprite(200,350,'paddle');
+    this.paddle = this.add.sprite(200,400,'paddle');
+    this.game.physics.arcade.enable(this.paddle);
     this.paddle.scale.x = 1.5;
     this.paddle.anchor.setTo(0.5);
+    this.paddle.body.allowGravity = false;
+    this.paddle.body.immovable = true;
   },
 
   update: function() {
+    this.game.physics.arcade.collide(this.ball, this.paddle);
   }
 };
 
